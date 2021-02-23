2e0d904a1da8c0fe097a06b47c9a1e53e64c4920
3TUSK <urey.s.knowledge@gmail.com>
2021-02-22 17:34:08 -0800

    Dynamic locations are now properly stored.
    Also serves as a real use case of "using concrete class as a capability".

--- src/main/java/org/teacon/signin/SignMeUp.java
+++ src/main/java/org/teacon/signin/SignMeUp.java
@@ -2,6 +2,7 @@ package org.teacon.signin;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.datafixers.util.Function4;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.command.arguments.BlockPosArgument;
@@ -9,8 +10,13 @@ import net.minecraft.command.arguments.ResourceLocationArgument;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.event.AddReloadListenerEvent;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -18,8 +24,10 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
+import org.teacon.signin.data.DynamicLocationStorage;
 import org.teacon.signin.data.GuideMapManager;
 import org.teacon.signin.data.Trigger;
+import org.teacon.signin.data.Waypoint;
 import org.teacon.signin.network.PartialUpdate;
 import org.teacon.signin.network.SyncGuideMap;
 import org.teacon.signin.network.TriggerActivation;
@@ -42,6 +50,7 @@ public class SignMeUp {
     }
 
     public static void setup(FMLCommonSetupEvent event) {
+        CapabilityManager.INSTANCE.register(DynamicLocationStorage.class, new DynamicLocationStorage.Serializer(), DynamicLocationStorage::new);
         channel.registerMessage(0, SyncGuideMap.class, SyncGuideMap::write, SyncGuideMap::new, SyncGuideMap::handle);
         channel.registerMessage(1, PartialUpdate.class, PartialUpdate::write, PartialUpdate::new, PartialUpdate::handle);
         channel.registerMessage(2, TriggerActivation.class, TriggerActivation::write, TriggerActivation::new, TriggerActivation::handle);
@@ -72,44 +81,82 @@ public class SignMeUp {
                                         .then(Commands.literal("actual")
                                                 .then(Commands.argument("pos", BlockPosArgument.blockPos()).executes(SignMeUp::setWaypointActualPos)))
                                         .then(Commands.literal("render")
-                                                .then(Commands.argument("pos", BlockPosArgument.blockPos()).executes(SignMeUp::setWaypointActualPos)))))));
+                                                .then(Commands.argument("pos", BlockPosArgument.blockPos()).executes(SignMeUp::setWaypointRenderPos)))))));
+    }
+
+    @SubscribeEvent
+    public static void attachCap(AttachCapabilitiesEvent<World> event) {
+        event.addCapability(new ResourceLocation("sign_up"), new DynamicLocationStorage.Holder());
     }
 
     private static int listMaps(CommandContext<CommandSource> context) {
+        context.getSource().sendFeedback(new StringTextComponent("WIP :("), false);
         return Command.SINGLE_SUCCESS;
     }
 
     private static int openSpecificMap(CommandContext<CommandSource> context) {
+        context.getSource().sendFeedback(new StringTextComponent("WIP :("), false);
         return Command.SINGLE_SUCCESS;
     }
 
     private static int openNearestMap(CommandContext<CommandSource> context) {
+        context.getSource().sendFeedback(new StringTextComponent("WIP :("), false);
         return Command.SINGLE_SUCCESS;
     }
 
     private static int listWaypoints(CommandContext<CommandSource> context) {
+        context.getSource().sendFeedback(new StringTextComponent("WIP :("), false);
         return Command.SINGLE_SUCCESS;
     }
 
     private static int getWaypointPos(CommandContext<CommandSource> context) {
+        context.getSource().sendFeedback(new StringTextComponent("WIP :("), false);
         return Command.SINGLE_SUCCESS;
     }
 
     private static int setWaypointActualPos(CommandContext<CommandSource> context) {
-        return Command.SINGLE_SUCCESS;
+        return setDynamicWaypointPosImpl(context, (store, id, world, pos) -> {
+            store.setActual(id, world, pos);
+            return Command.SINGLE_SUCCESS;
+        });
     }
 
+
     private static int setWaypointRenderPos(CommandContext<CommandSource> context) {
-        return Command.SINGLE_SUCCESS;
+        return setDynamicWaypointPosImpl(context, (store, id, world, pos) -> {
+            store.setRendering(id, world, pos);
+            return Command.SINGLE_SUCCESS;
+        });
+    }
+
+    private static int setDynamicWaypointPosImpl(CommandContext<CommandSource> context, Function4<DynamicLocationStorage, ResourceLocation, World, BlockPos, Integer> handler) {
+        final CommandSource src = context.getSource();
+        final World world = src.getWorld();
+        final BlockPos pos = context.getArgument("pos", BlockPos.class);
+        final ResourceLocation id = context.getArgument("id", ResourceLocation.class);
+        final Waypoint wp = MANAGER.findWaypoint(id);
+        if (wp != null) {
+            if (wp.hasDynamicLocation()) {
+                return world.getCapability(DynamicLocationStorage.CAP).map(store -> handler.apply(store, id, world, pos)).orElse(-1);
+            } else {
+                src.sendErrorMessage(new StringTextComponent("Error: waypoint " + id + " is static"));
+                return -1;
+            }
+        } else {
+            src.sendErrorMessage(new StringTextComponent("Error: waypoint " + id + " does not exist"));
+            return -1;
+        }
     }
 
     public static void trigger(ServerPlayerEntity player, ResourceLocation triggerId) {
         final Trigger trigger = MANAGER.findTrigger(triggerId);
-        if (trigger != null) {
+        if (trigger != null && trigger.isVisibleTo(player)) {
             final MinecraftServer server = player.getServer();
             if (server != null) {
                 server.getCommandManager().handleCommand(player.getCommandSource().withPermissionLevel(2), trigger.command);
             }
+        } else {
+            player.sendStatusMessage(new StringTextComponent("You seemed to click the void just now..."), true);
         }
     }
 }
---
+++ src/main/java/org/teacon/signin/data/DynamicLocationStorage.java
@@ -0,0 +1,114 @@
+package org.teacon.signin.data;
+
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.INBT;
+import net.minecraft.nbt.NBTDynamicOps;
+import net.minecraft.util.Direction;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.GlobalPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.common.util.LazyOptional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DynamicLocationStorage {
+
+    private static final Logger LOGGER = LogManager.getLogger("SignMeUp");
+
+    @CapabilityInject(DynamicLocationStorage.class)
+    public static Capability<DynamicLocationStorage> CAP;
+
+    final Map<ResourceLocation, GlobalPos> pos = new HashMap<>();
+    final Map<ResourceLocation, GlobalPos> renderPos = new HashMap<>();
+
+    public GlobalPos getActual(ResourceLocation id) {
+        return pos.get(id);
+    }
+
+    public GlobalPos getRendering(ResourceLocation id) {
+        return renderPos.get(id);
+    }
+
+    public void setActual(ResourceLocation id, World world, BlockPos pos) {
+        this.pos.put(id, GlobalPos.getPosition(world.getDimensionKey(), pos));
+    }
+
+    public void setRendering(ResourceLocation id, World world, BlockPos pos) {
+        this.renderPos.put(id, GlobalPos.getPosition(world.getDimensionKey(), pos));
+    }
+
+    public static final class Serializer implements Capability.IStorage<DynamicLocationStorage> {
+
+        @Override
+        public INBT writeNBT(Capability<DynamicLocationStorage> capability, DynamicLocationStorage instance, Direction side) {
+            return writeNBTImpl(instance);
+        }
+
+        @Override
+        public void readNBT(Capability<DynamicLocationStorage> capability, DynamicLocationStorage instance, Direction side, INBT nbt) {
+            readNBTImpl(instance, nbt);
+        }
+
+        public static CompoundNBT writeNBTImpl(DynamicLocationStorage instance) {
+            final CompoundNBT data = new CompoundNBT();
+            final CompoundNBT actualPoses = new CompoundNBT();
+            final CompoundNBT renderPoses = new CompoundNBT();
+            instance.pos.forEach((id, pos) -> collect(id, pos, actualPoses));
+            instance.renderPos.forEach((id, pos) -> collect(id, pos, renderPoses));
+            data.put("actual", actualPoses);
+            data.put("render", renderPoses);
+            return data;
+        }
+
+        public static void readNBTImpl(DynamicLocationStorage instance, INBT nbt) {
+            if (nbt instanceof CompoundNBT) {
+                final CompoundNBT data = (CompoundNBT) nbt;
+                final CompoundNBT actualPoses = data.getCompound("actual");
+                final CompoundNBT renderPoses = data.getCompound("render");
+                actualPoses.keySet().forEach(id -> parse(actualPoses, id, instance.pos));
+                renderPoses.keySet().forEach(id -> parse(renderPoses, id, instance.renderPos));
+            }
+        }
+
+        private static void collect(ResourceLocation id, GlobalPos pos, CompoundNBT dst) {
+            dst.put(id.toString(), GlobalPos.CODEC.encodeStart(NBTDynamicOps.INSTANCE, pos).getOrThrow(false, LOGGER::warn));
+        }
+
+        private static void parse(CompoundNBT src, String id, Map<ResourceLocation, GlobalPos> dst) {
+            dst.put(new ResourceLocation(id), GlobalPos.CODEC.parse(NBTDynamicOps.INSTANCE, src.get(id)).getOrThrow(false, LOGGER::warn));
+        }
+    }
+
+    public static final class Holder implements ICapabilityProvider, INBTSerializable<CompoundNBT> {
+
+        private final DynamicLocationStorage storage = new DynamicLocationStorage();
+        private final LazyOptional<DynamicLocationStorage> wrapped = LazyOptional.of(() -> this.storage);
+
+        @Nonnull
+        @Override
+        public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+            return cap == CAP ? this.wrapped.cast() : LazyOptional.empty();
+        }
+
+        @Override
+        public CompoundNBT serializeNBT() {
+            return Serializer.writeNBTImpl(this.storage);
+        }
+
+        @Override
+        public void deserializeNBT(CompoundNBT nbt) {
+            Serializer.readNBTImpl(this.storage, nbt);
+        }
+    }
+
+}
